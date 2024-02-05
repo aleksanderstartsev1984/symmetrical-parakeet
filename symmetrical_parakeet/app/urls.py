b195a0c2865ab9c84091108ac96115a1739fac05
aleksanderstartsev1984 <strami@yandex.ru>
2024-02-05 15:31:09 +0700

    add send_message

--- symmetrical_parakeet/app/urls.py
+++ symmetrical_parakeet/app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from app.views import index, share
+from app.views import index, share, send_message_to_telegram
 
 
 app_name = 'app'
@@ -8,4 +8,5 @@ app_name = 'app'
 urlpatterns = [
     path('', index, name='index'),
     path('share/', share, name='share'),
+    path('send/<str:text>/', send_message_to_telegram, name='send'),
 ]
--- symmetrical_parakeet/app/views.py
+++ symmetrical_parakeet/app/views.py
@@ -1,6 +1,8 @@
 # flake8:noqa
+from pprint import pprint
+
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponseRedirect
 
 
 def index(request):
@@ -8,4 +10,16 @@ def index(request):
 
 
 def share(request):
-    return JsonResponse({"url": """https://api.telegram.org/bot6216510134:AAFubMLITpcM02wVJu3WNvBKEvtN5KXxbT8/sendmessage?chat_id=-1001262768687&text=business-card:     """})
+    res = str(request.headers)
+
+    print('##################################################################')
+    pprint(str(res))
+    print('##################################################################')
+
+    url = {"url": "https://api.telegram.org/bot6216510134:AAFubMLITpcM02wVJu3WNvBKEvtN5KXxbT8/sendmessage?chat_id=-1001262768687&text=business-card:     "}
+    url['response'] = str(res)
+    return JsonResponse(url)
+
+
+def send_message_to_telegram(request, text):
+    return HttpResponseRedirect(f"https://api.telegram.org/bot6216510134:AAFubMLITpcM02wVJu3WNvBKEvtN5KXxbT8/sendmessage?chat_id=-1001262768687&text=business-card:     {text}")
